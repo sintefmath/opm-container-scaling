c76886b653dfb68a700d673961dcf994ded1c290
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2022-12-01 16:39:30 +0100

    fixes to walltime arguments

--- utils/walltime_argument.py
+++ utils/walltime_argument.py
@@ -1,6 +1,8 @@
 import argparse
 import datetime
 import re
+
+
 class Walltime:
     def __init__(self, *, hours, minutes, seconds):
         self.hours = hours
@@ -15,8 +17,8 @@ def make_time(time_as_string):
     match = re.search(r'(\d+):(\d+):(\d+)', time_as_string)
     if not match:
         raise ValueError(f"Unrecognized walltime format {time_as_string}, expected 'HH:MM:SS'")
-    return Walltime(int(match.group(1)), int(match.group(2)), int(match.group(3)))
-    
+    return Walltime(hours=int(match.group(1)), minutes=int(match.group(2)), seconds=int(match.group(3)))
+
 
 def add_walltime_argument(parser: argparse.ArgumentParser):
     parser.add_argument('--walltime', type=make_time, default="01:00:00")
