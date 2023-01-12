ae9c267122501123581f657d81c89016b6e10e56
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2023-01-12 11:08:11 +0100

    fixed extra modules

--- clusters/it4i/barbora.py
+++ clusters/it4i/barbora.py
@@ -4,5 +4,5 @@ from .base_it4i import BaseIT4I
 
 class Barbora(BaseIT4I):
 
-    def __init__(self, account_id, container, runner = subprocess.run, extra_arguments=[]):
-        super().__init__(account_id, container, 36, runner=runner, extra_arguments=extra_arguments)
+    def __init__(self, account_id, container, runner = subprocess.run, extra_arguments=[], extra_modules=[]):
+        super().__init__(account_id, container, 36, runner=runner, extra_arguments=extra_arguments, extra_modules=extra_modules)
--- clusters/it4i/karolina.py
+++ clusters/it4i/karolina.py
@@ -4,5 +4,5 @@ from .base_it4i import BaseIT4I
 
 class Karolina(BaseIT4I):
 
-    def __init__(self, account_id, container, runner = subprocess.run, extra_arguments=[]):
-        super().__init__(account_id, container, 128, runner=runner, extra_arguments=extra_arguments)
+    def __init__(self, account_id, container, runner = subprocess.run, extra_arguments=[], extra_modules=[]):
+        super().__init__(account_id, container, 128, runner=runner, extra_arguments=extra_arguments, extra_modules=extra_modules)
