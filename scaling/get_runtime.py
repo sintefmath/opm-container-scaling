f3d5346d0f60cba5ae75054dcefa05cd8cfbaeaf
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2023-01-24 13:12:09 +0100

    Implemented effienciency

--- scaling/get_runtime.py
+++ scaling/get_runtime.py
@@ -1,7 +1,8 @@
 import re
-
+import os
 
 def get_runtime(filename):
+
     with open(filename) as f:
         for line in f:
             if match := re.search(r'Total time \(seconds\):\s+(\d+.\d+)', line):
@@ -9,10 +10,22 @@ def get_runtime(filename):
         else:
             raise Exception(f"Runtime not found in {filename}.")
         
-def get_runtimes(configuration):
+def get_runtimes(configuration, skip_missing=False):
     runtimes = []
     for filename in configuration['output_filenames']:
-        runtimes.append(get_runtime(filename))
+        if os.path.exists(filename):
+            try:
+                runtimes.append(get_runtime(filename))
+            except Exception as e:
+                if skip_missing:
+                    runtimes.append(-1)
+                else:
+                    raise e
+        else:
+            if skip_missing:
+                runtimes.append(-1)
+            else:
+                raise Exception(f"File not found: {filename}")
     
     return runtimes
     
--- scaling/plot_scalings.py
+++ scaling/plot_scalings.py
@@ -11,13 +11,27 @@ def plot_scalings(configuration_files, skip_missing=True, save_to_file=None, sho
     for n, configuration_file in enumerate(configuration_files):
         with open(configuration_file) as f:
             configuration = json.load(f)
-    
-        runtimes = get_runtimes(configuration)
+        runtimes = get_runtimes(configuration, skip_missing=skip_missing)
 
         number_of_processes_list = np.array(list(map(int, configuration['number_of_processes_list'])), dtype=np.float64)
+
+        runtimes_procs = zip(runtimes, number_of_processes_list)
+        print(runtimes)
+        filtered_runtimes_procs = list(filter(lambda p: p[0] >= 0, runtimes_procs))
+        runtimes = np.array([p[0] for p in filtered_runtimes_procs])
+        number_of_processes_list = np.array([p[1] for p in filtered_runtimes_procs])
+
+        plt.figure(1)
         plt.loglog(number_of_processes_list,
                    runtimes, symbols[n%len(symbols)], label=configuration_file)
-        
+
+        plt.figure(2)
+        ideal_runtimes = runtimes[0] * number_of_processes_list[0] / number_of_processes_list
+        efficiency = ideal_runtimes / runtimes
+        plt.loglog(number_of_processes_list,
+                   efficiency, symbols[n%len(symbols)], label=configuration_file)
+
+    plt.figure(1)    
     plt.loglog(number_of_processes_list, 
                runtimes[0] * number_of_processes_list[0] * number_of_processes_list**(-1),
                '--', label='Ideal')
@@ -28,8 +42,23 @@ def plot_scalings(configuration_files, skip_missing=True, save_to_file=None, sho
     plt.xscale('log', base=2)
     plt.yscale('log', base=2)
     plt.legend()
+     
+    if save_to_file is not None:
+        plt.savefig(save_to_file + "_runtime.png")
+
+    plt.figure(2)
+    plt.loglog(number_of_processes_list, 
+               np.ones_like(number_of_processes_list),
+               '--', label='Ideal')
+    
+    plt.ylabel('Efficiency')
+    plt.xlabel('Number of processes')
+    plt.grid(True)
+    plt.xscale('log', base=2)
+    plt.yscale('linear')
+    plt.legend()
     if show:
         plt.show()
         
     if save_to_file is not None:
-        plt.savefig(save_to_file)
+        plt.savefig(save_to_file + "_effiency.png")
